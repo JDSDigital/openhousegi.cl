f9e2206c2b906882a8e28e7bf5bf8d62b94ba1a1
Daniel Sosa <jdsosa@gmail.com>
2018-06-14 20:49:20 +0200

    Add rev slider with videos to home

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,67 +7,185 @@ use yii\helpers\Html;
 $this->title = 'Open House';
 ?>
 
-<div style="position:relative;height:100vh;background-image:url('<?= Yii::getAlias('@web') . '/images/blue-sky-building.jpg' ?>');background-attachment: fixed;background-position: center;background-repeat: no-repeat;background-size: cover;">
-	<?= Html::img(Yii::getAlias('@web') . '/images/logo/logo-full.png', ['class' => 'logo-rev-slider wow fadeIn', 'data-wow-delay'=>'1s']) ?>
-</div>
+<div id="rev_slider_110_1_wrapper" class="rev_slider_wrapper fullscreen-container" data-alias="rotating-words82" style="background-color:#e5e5e5;padding:0px;">
+	<!-- START REVOLUTION SLIDER 5.0.7 fullscreen mode -->
+	<div id="rev_slider_110_1" class="rev_slider fullscreenbanner tiny_bullet_slider" style="display:none;" data-version="5.0.7">
+		<ul class="js-scroll-nav">	<!-- SLIDE  -->
+			<li data-index="rs-333" data-transition="fade" data-slotamount="default"  data-easein="Power3.easeInOut" data-easeout="Power3.easeInOut" data-masterspeed="1500"  data-rotate="0"  data-saveperformance="off"  data-title="Intro" data-description="">
+				<!-- MAIN IMAGE -->
+				<img src="<?= Yii::getAlias('@web') . '/video/office_cover.jpg' ?>"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+				<!-- LAYERS -->
 
-<div class="bg-white">
+				<!-- BACKGROUND VIDEO LAYER -->
+				<div class="rs-background-video-layer"
+					data-forcerewind="on"
+					data-volume="mute"
+					data-videowidth="100%"
+					data-videoheight="100%"
+					data-videomp4="<?= Yii::getAlias('@web') . '/video/Working-Space.mp4' ?>"
+					data-videopreload="preload"
+					data-videoloop="loopandnoslidestop"
+					data-forceCover="1"
+					data-aspectratio="16:9"
+					data-autoplay="true"
+					data-nextslideatend="true"
+				></div>
+				<!-- LAYER NR. 1 -->
+				<div class="tp-caption tp-shape tp-shapewrapper   rs-parallaxlevel-0"
+					 id="slide-333-layer-10"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+								data-width="full"
+					data-height="full"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
 
-    <ul class="grid portfolio filterArea list-unstyled text-center wow fadeInUp">
+					 data-transform_in="opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="0"
+					data-basealign="slide"
+					data-responsive_offset="on"
+					data-responsive="off"
 
-				<!-- Portfolio Item 1 -->
+					style="z-index: 5;background-color:rgba(41, 46, 49, 0.5);border-color:rgba(0, 0, 0, 0.50);">
+				</div>
 
-				<li class="webdesign">
-					<a href="#">
-						<figure class="effect-3">
-							<?= Html::img(Yii::getAlias('@web') . '/images/app-bg.jpg', ['class' => 'img-responsive']) ?>
-							<figcaption>
-								<div class="fig-caption-center">
-									<div>
-										<h3>Lorem Ipsum</h3>
-									</div>
-								</div>
-							</figcaption>
-						</figure>
-				   </a>
-				</li>
+				<!-- LAYER NR. 2 -->
+				<div class="tp-caption RotatingWords-TitleWhite text-main tp-resizeme toblur rs-parallaxlevel-0"
+					 id="slide-333-layer-1"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+					data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
 
-				<!-- Portfolio Item 2 -->
+					 data-transform_in="y:100px;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="750"
+					data-splitin="none"
+					data-splitout="none"
+					data-responsive_offset="on"
 
-				<li class="appicon">
-					<a href="#">
-						<figure class="effect-3">
-							<?= Html::img(Yii::getAlias('@web') . '/images/app-bg-2.jpg', ['class' => 'img-responsive']) ?>
-							<figcaption>
-								<div class="fig-caption-center">
-									<div>
-										<h3>Lorem Ipsum</h3>
-									</div>
-								</div>
-							</figcaption>
-						</figure>
-					</a>
-				</li>
 
-				<!-- Portfolio Item 3 -->
+					style="z-index: 6; white-space: nowrap;">
+					<?= Html::img(Yii::getAlias('@web') . '/images/logo/logo-full.png', ['class' => 'logo-rev-slider']) ?>
+				</div>
 
-				<li class="iosappui">
-					<a href="#">
-						<figure class="effect-3">
-							<?= Html::img(Yii::getAlias('@web') . '/images/apple-products.jpg', ['class' => 'img-responsive']) ?>
-							<figcaption>
-								<div class="fig-caption-center">
-									<div>
-										<h3>Lorem Ipsum</h3>
-									</div>
-								</div>
-							</figcaption>
-						</figure>
-					</a>
-				</li>
+				<!-- LAYER NR. 7 -->
+				<div class="tp-caption RotatingWords-Button rev-btn bg-color1 text-main rs-parallaxlevel-0"
+					 id="slide-333-layer-7"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['127','97','90','90']"
+								data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+						data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
+						data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
 
-			</ul><!-- End of Portfolio 3 Grid -->
-</div>
+					 data-transform_in="sX:1;sY:1;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="y:50px;opacity:0;s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+					data-start="2000"
+					data-splitin="none"
+					data-splitout="none"
+					data-actions='[{"event":"click","action":"scrollbelow","offset":"0px"}]'
+					data-responsive_offset="on"
+					data-responsive="off"
+
+					style="z-index: 11; white-space: nowrap;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">VER MAS
+				</div>
+			</li>
+			<li data-index="rs-334" data-transition="fade" data-slotamount="default"  data-easein="Power3.easeInOut" data-easeout="Power3.easeInOut" data-masterspeed="1500"  data-rotate="0"  data-saveperformance="off"  data-title="Intro" data-description="">
+				<!-- MAIN IMAGE -->
+				<img src="<?= Yii::getAlias('@web') . '/video/office_cover.jpg' ?>"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+				<!-- LAYERS -->
+
+				<!-- BACKGROUND VIDEO LAYER -->
+				<div class="rs-background-video-layer"
+					data-forcerewind="on"
+					data-volume="mute"
+					data-videowidth="100%"
+					data-videoheight="100%"
+					data-videomp4="<?= Yii::getAlias('@web') . '/video/Working-Space.mp4' ?>"
+					data-videopreload="preload"
+					data-videoloop="loopandnoslidestop"
+					data-forceCover="1"
+					data-aspectratio="16:9"
+					data-autoplay="true"
+					data-nextslideatend="true"
+				></div>
+				<!-- LAYER NR. 1 -->
+				<div class="tp-caption tp-shape tp-shapewrapper   rs-parallaxlevel-0"
+					 id="slide-334-layer-10"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+								data-width="full"
+					data-height="full"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+
+					 data-transform_in="opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="0"
+					data-basealign="slide"
+					data-responsive_offset="on"
+					data-responsive="off"
+
+					style="z-index: 5;background-color:rgba(41, 46, 49, 0.5);border-color:rgba(0, 0, 0, 0.50);">
+				</div>
+
+				<!-- LAYER NR. 2 -->
+				<div class="tp-caption RotatingWords-TitleWhite text-main tp-resizeme toblur rs-parallaxlevel-0"
+					 id="slide-334-layer-1"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+					data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+
+					 data-transform_in="y:100px;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="750"
+					data-splitin="none"
+					data-splitout="none"
+					data-responsive_offset="on"
+
+
+					style="z-index: 6; white-space: nowrap;">
+					<?= Html::img(Yii::getAlias('@web') . '/images/logo/logo-full.png', ['class' => 'logo-rev-slider']) ?>
+				</div>
+
+				<!-- LAYER NR. 7 -->
+				<div class="tp-caption RotatingWords-Button rev-btn bg-color1 text-main rs-parallaxlevel-0"
+					 id="slide-334-layer-7"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['127','97','90','90']"
+								data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+						data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
+						data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
+
+					 data-transform_in="sX:1;sY:1;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="y:50px;opacity:0;s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+					data-start="2000"
+					data-splitin="none"
+					data-splitout="none"
+					data-actions='[{"event":"click","action":"scrollbelow","offset":"0px"}]'
+					data-responsive_offset="on"
+					data-responsive="off"
+
+					style="z-index: 11; white-space: nowrap;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">VER MAS
+				</div>
+			</li>
+		</ul>
+		<div class="tp-bannertimer tp-bottom" style="height: 5px; background: #fff;"></div>
+	</div>
+</div><!-- END REVOLUTION SLIDER -->
+
 
 <div class="container mt50 mb50 bg-white">
   <div class="row wow fadeInUp">
--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -5,3 +5,93 @@ Version: 1.0
 Created By: Amazyne Themes
 
 *********************************/
+
+var tpj=jQuery;
+var revapi110;
+tpj(document).ready(function() {
+  if(tpj("#rev_slider_110_1").revolution == undefined){
+    revslider_showDoubleJqueryError("#rev_slider_110_1");
+  }else{
+    revapi110 = tpj("#rev_slider_110_1").show().revolution({
+      // sliderType:"hero",
+      jsFileLocation:"third-party/revolution/js/",
+      sliderLayout:"fullscreen",
+      dottedOverlay:"none",
+      delay:5000,
+      navigation: {
+        arrows: {
+
+          enable: true,
+          style: 'hesperiden',
+          tmp: '',
+          rtl: false,
+          hide_onleave: false,
+          hide_onmobile: true,
+          hide_under: 0,
+          hide_over: 9999,
+          hide_delay: 200,
+          hide_delay_mobile: 1200,
+
+          left: {
+              container: 'slider',
+              h_align: 'left',
+              v_align: 'center',
+              h_offset: 20,
+              v_offset: 0
+          },
+
+          right: {
+              container: 'slider',
+              h_align: 'right',
+              v_align: 'center',
+              h_offset: 20,
+              v_offset: 0
+          }
+
+      },
+        // bullets: {
+        //   enable: true,
+        //   hide_onmobile: false,
+        //   style: "bullet-bar",
+        //   hide_onleave: false,
+        //   direction: "horizontal",
+        //   h_align: "center",
+        //   v_align: "bottom",
+        //   h_offset: 0,
+        //   v_offset: 50,
+        //   space: 5,
+        //   tmp: ''
+        // }
+      },
+      responsiveLevels:[1240,1024,778,778],
+      gridwidth:[1240,1024,778,480],
+      gridheight:[600,500,400,300],
+      lazyType:"none",
+      // parallax: {
+      //   type:"mouse",
+      //   origo:"slidercenter",
+      //   speed:2000,
+      //   levels:[2,3,4,5,6,7,12,16,10,50],
+      // },
+      shadow:0,
+      spinner: "off",
+      stopLoop: "off",
+      stopAfterLoops: -1,
+      stopAtSlide: -1,
+      shuffle: "off",
+      autoHeight:"off",
+      fullScreenAlignForce:"off",
+      fullScreenOffsetContainer: "",
+      fullScreenOffset: "",
+      hideThumbsOnMobile:"off",
+      hideSliderAtLimit:0,
+      hideCaptionAtLimit:0,
+      hideAllCaptionAtLilmit:0,
+      debugMode:false,
+      fallbacks: {
+        simplifyAll:"off",
+        disableFocusListener:false,
+      }
+    });
+  }
+});	/*ready*/
