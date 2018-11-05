48563f00d7527fc272fd445333d5e437b9628623
Daniel Sosa <jdsosa@gmail.com>
2018-11-05 22:14:34 +0100

    Add Santiago City picture to home

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -103,6 +103,77 @@ $this->title = 'Open House';
 					style="z-index: 11; white-space: nowrap;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">VER MAS
 				</div>
 			</li>
+			<li data-index="rs-334" data-transition="fade" data-slotamount="default"  data-easein="Power3.easeInOut" data-easeout="Power3.easeInOut" data-masterspeed="1500"  data-rotate="0"  data-saveperformance="off"  data-title="Intro" data-description="">
+				<!-- MAIN IMAGE -->
+				<img src="<?= Yii::getAlias('@web') . '/video/santiago-cover.jpg' ?>"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+				<!-- LAYERS -->
+
+				<!-- LAYER NR. 1 -->
+				<div class="tp-caption tp-shape tp-shapewrapper   rs-parallaxlevel-0"
+					 id="slide-334-layer-10"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+								data-width="full"
+					data-height="full"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+
+					 data-transform_in="opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="0"
+					data-basealign="slide"
+					data-responsive_offset="on"
+					data-responsive="off"
+
+					style="z-index: 5;background-color:rgba(41, 46, 49, 0.5);border-color:rgba(0, 0, 0, 0.50);">
+				</div>
+
+				<!-- LAYER NR. 2 -->
+				<div class="tp-caption text-main rs-parallaxlevel-0"
+					 id="slide-334-layer-1"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
+					data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+
+					 data-transform_in="y:100px;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="s:300;s:300;"
+					data-start="750"
+					data-splitin="none"
+					data-splitout="none"
+					data-responsive_offset="on"
+
+
+					style="z-index: 6; white-space: nowrap;">
+					<?= Html::img(Yii::getAlias('@web') . '/images/logo/logo-full.png', ['class' => 'logo-rev-slider']) ?>
+				</div>
+
+				<!-- LAYER NR. 7 -->
+				<div class="tp-caption RotatingWords-Button rev-btn bg-color1 text-main rs-parallaxlevel-0"
+					 id="slide-334-layer-7"
+					 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+					 data-y="['middle','middle','middle','middle']" data-voffset="['127','97','90','90']"
+								data-width="none"
+					data-height="none"
+					data-whitespace="nowrap"
+					data-transform_idle="o:1;"
+						data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:300;e:Power1.easeInOut;"
+						data-style_hover="c:rgba(255, 255, 255, 1.00);bc:rgba(255, 255, 255, 1.00);cursor:pointer;"
+
+					 data-transform_in="sX:1;sY:1;opacity:0;s:1500;e:Power3.easeInOut;"
+					 data-transform_out="y:50px;opacity:0;s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+					data-start="2000"
+					data-splitin="none"
+					data-splitout="none"
+					data-actions='[{"event":"click","action":"scrollbelow","offset":"0px"}]'
+					data-responsive_offset="on"
+					data-responsive="off"
+
+					style="z-index: 11; white-space: nowrap;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;">VER MAS
+				</div>
+			</li>
 		</ul>
 		<div class="tp-bannertimer tp-bottom" style="height: 5px; background: #fff;"></div>
 	</div>
@@ -121,7 +192,7 @@ $this->title = 'Open House';
 			<br>
 			<div class="row product">
 				<?php foreach ($featured as $property) : ?>
-					<?php
+				<?php
 			      $url = ($property->cover)
 			      ? Yii::getAlias('@web') . '/images/properties/thumbs/' . $property->cover->file
 			      : Yii::getAlias('@web') . '/images/logo/logo.jpg';
